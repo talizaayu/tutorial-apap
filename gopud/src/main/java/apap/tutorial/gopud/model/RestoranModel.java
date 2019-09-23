3dc8877440f7692f77b936bc0a2ff0bd7c771510
Taliza Ayu <taliza_ayu@yahoo.com>
2019-09-23 13:43:06 +0700

    feat:tutorial-2 pull

--- gopud/src/main/java/apap/tutorial/gopud/model/RestoranModel.java
+++ gopud/src/main/java/apap/tutorial/gopud/model/RestoranModel.java
@@ -6,6 +6,7 @@ public class RestoranModel {
     private String alamat;
     private Integer nomorTelepon;
 
+
     public RestoranModel(String idRestoran, String nama, String alamat, Integer nomorTelepon) {
         this.idRestoran = idRestoran;
         this.nama = nama;
