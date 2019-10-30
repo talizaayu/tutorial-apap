235218b51b9063ea7af81d9ef0c8786b0b06ea1b
Taliza Ayu <taliza_ayu@yahoo.com>
2019-10-30 23:02:51 +0700

    feat: add RestoranDetail.java file

---
+++ gopud/src/main/java/apap/tutorial/gopud/rest/RestoranDetail.java
@@ -0,0 +1,42 @@
+package apap.tutorial.gopud.rest;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RestoranDetail {
+    private String status;
+
+    @JsonProperty("restoran-license")
+    private Integer restaurantLicense;
+
+    @JsonProperty("valid-until")
+    private Date validUntil;
+    
+    public Date getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(Date validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    public Integer getRestaurantLicense() {
+        return restaurantLicense;
+    }
+
+    public void setRestaurantLicense(Integer restaurantLicense) {
+        this.restaurantLicense = restaurantLicense;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
