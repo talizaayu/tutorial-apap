d5a501b62211b190821a5c29e8ddde6369607f36
Taliza Ayu <taliza_ayu@yahoo.com>
2019-11-06 14:01:55 +0700

    feat: modify restoran controller for homepage

--- gopud/src/main/java/apap/tutorial/gopud/controller/RestoranController.java
+++ gopud/src/main/java/apap/tutorial/gopud/controller/RestoranController.java
@@ -27,10 +27,10 @@ public class RestoranController {
     @Autowired
     private MenuService menuService;
 
-    @RequestMapping("/")
-    public String home() {
-        return "home";
-    }
+    // @RequestMapping("/")
+    // public String home() {
+    //     return "home";
+    // }
 
     @RequestMapping(value = "/restoran/add", method = RequestMethod.GET)
     public String addRestoranFormPage(Model model) {
