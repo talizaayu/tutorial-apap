4d70bc2fa2f45133f224c90390009c20c53ab942
Taliza Ayu <taliza_ayu@yahoo.com>
2019-11-28 12:23:52 +0700

    feat : revert edit

--- gopud/src/main/java/apap/tutorial/gopud/restcontroller/RestoranRestController.java
+++ gopud/src/main/java/apap/tutorial/gopud/restcontroller/RestoranRestController.java
@@ -17,7 +17,6 @@ import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:3000")
 public class RestoranRestController {
     @Autowired
     private RestoranRestService restoranRestService;
