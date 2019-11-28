93f28fd5be1fd17e82aa46c13f624aee6929ba40
Taliza Ayu <taliza_ayu@yahoo.com>
2019-11-28 21:37:52 +0700

    feat: try backend

--- frontend-tutorial9/src/containers/Restorans/Restorans.js
+++ frontend-tutorial9/src/containers/Restorans/Restorans.js
@@ -2,14 +2,54 @@ import React, { Component } from 'react';
 import Restoran from '../../components/Restoran/Restoran';
 import classes from "./Restorans.module.css";
 import Axios from '../../axios-restoran.js';
+import Modal from '../../components/UI/Modal/Modal';
+import Button from "../../components/UI/Button/Button";
 
 class Restorans extends Component{
     constructor(props){
         super(props);
         this.state = {
             restorans: [],
+            isCreate: false,
             isLoading: true,
+            nama: "",
+            alamat: "",
+            nomorTelepon: "",
+            rating: ""
         }
+    }
+
+    addRestoranHandler = () => {
+        this.setState({ isCreate: true });
+    }
+
+    canceledHandler = () => {
+        this.setState({ isCreate: false });
+    }
+
+    changeHandler = event => {
+        // name dari prop name di input
+        const { name, value } = event.target;
+        this.setState({ [name]: value});
+    }
+
+    submitAddRestoranHandler = event => {
+        event.preventDefault();
+        this.setState({ isLoading: true});
+        this.addRestoran();
+        this.canceledHandler();
+    }
+
+    async addRestoran(){
+        const restoranToAdd = {
+            nama: this.state.nama,
+            alamat: this.state.alamat,
+            nomorTelepon: this.state.nomorTelepon,
+            rating: this.state.rating
+        };
+
+        await Axios.post("/restoran", restoranToAdd);
+        await this.loadRestorans();
     }
 
     componentDidMount() {
@@ -27,23 +67,81 @@ class Restorans extends Component{
         this.setState({
             restorans: fetchedRestorans
         });
-    }
+    };
 
     render(){
         return(
             <React.Fragment>
+                <Modal show={this.state.isCreate || this.state.isEdit}
+                    modalClosed={this.canceledHandler}>
+                    {this.renderForm()}
+                </Modal>
                 <div className={classes.Title}>All Restorans</div>
+                <div className={classes.ButtonLayout}>
+                    <button
+                        className={classes.AddRestoranButton}
+                        onClick={this.addRestoranHandler}
+                    >
+                        + Add New Restoran
+                    </button>
+                </div>
                 <div className={classes.Restorans}>
-                    {this.state.restorans.map(restoran => 
+                    {this.state.restorans &&
+                     this.state.restorans.map(restoran => 
                         <Restoran
-                        key={restoran.id}
-                        nama={restoran.nama}
-                        alamat={restoran.alamat}
-                        nomorTelepon={restoran.nomorTelepon}
+                            key={restoran.id}
+                            nama={restoran.nama}
+                            alamat={restoran.alamat}
+                            nomorTelepon={restoran.nomorTelepon}
                         />
-                        )}
+                    )}
                 </div>
             </React.Fragment>
+        );
+    }
+
+    renderForm() {
+        return (
+            <form>
+                <input
+                className={classes.Input}
+                name="nama"
+                type="text"
+                placeholder="Nama"
+                value={this.state.nama}
+                onChange={this.changeHandler}
+                />
+                <input
+                className={classes.Input}
+                name="nomorTelepon"
+                type="number"
+                placeholder="Nomor Telepon"
+                value={this.state.nomorTelepon}
+                onChange={this.changeHandler}
+                />
+                <textarea
+                className={classes.TextArea}
+                name="alamat"
+                type="text"
+                placeholder="Alamat"
+                value={this.state.alamat}
+                onChange={this.changeHandler}
+                />
+                <input
+                className={classes.Input}
+                name="rating"
+                type="number"
+                placeholder="Rating"
+                value={this.state.rating}
+                onChange={this.changeHandler}
+                />
+                <Button btnType="Danger" onClick={this.canceledHandler}>
+                    CANCEL
+                </Button>
+                <Button btnType="Success" onClick={this.submitAddRestoranHandler}>
+                    SUBMIT
+                </Button>
+            </form>
         );
     }
 }
--- gopud/src/main/java/apap/tutorial/gopud/security/WebSecurityConfig.java
+++ gopud/src/main/java/apap/tutorial/gopud/security/WebSecurityConfig.java
@@ -28,6 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .loginPage("/login").permitAll()
             .and()
             .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
+            // .and().cors();
     }
 
     @Bean
